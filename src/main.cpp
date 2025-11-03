edcb6c578425e6cda257f8931429ab14fa60a8dd
HQ1412 <82790436+HQ1412@users.noreply.github.com>
2025-11-02 18:20:51 -0600

    C Major Scale Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "Wire.h"
+#include <Arduino.h>
 
-int main()
-{
-   std::cout << "Hello world!";
-   return 0;
+const int buzzerPin = 32; // buzzer is on GPIO 32
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  // Plays a C4 note for 300ms
+  tone(buzzerPin, 261.6, 300);
+  tone(buzzerPin, 293.7, 300);
+  tone(buzzerPin, 329.6, 300);
+  tone(buzzerPin, 349.2, 300);
+  tone(buzzerPin, 392.0, 300);
+  tone(buzzerPin, 440.0, 300);
+  tone(buzzerPin, 493.9, 300);
+  tone(buzzerPin, 523.3, 300);
+  delay(350); // Wait for sound to finish and a short pause
 }
